b0a6c403a7d8f75692de94137462766ee38d3948
Tatsuya Hashimura <luru84@gmail.com>
2020-08-16 15:14:41 +0900

    [add]クロスプラットフォームアプリ(XMemo) p190

---
+++ XMEMO/XMEMO/Memo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace XMEMO
+{
+
+    /// <summary>
+    /// メモクラス、メモ1枚のデータ
+    /// </summary>
+    public class Memo
+    {
+        public Memo()
+        {
+        }
+
+        /// <summary>
+        /// メモ時間
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// メモタイトル
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// メモ内容
+        /// </summary>
+        public string Text { get; set; }
+
+    }
+}
---
+++ XMEMO/XMEMO/MemoHolder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace XMEMO
+{
+    /// <summary>
+    /// メモホルダー、メモの集合体
+    /// </summary>
+    public class MemoHolder
+    {
+        public MemoHolder()
+        {
+        }
+
+        /// <summary>
+        /// 新インスタンスを取得するゲッター
+        /// </summary>
+        public static MemoHolder Current { get; } = new MemoHolder();
+
+        /// <summary>
+        /// メモインスタンスを保持
+        /// </summary>
+        public Memo Memo { get; set; }
+
+    }
+}
