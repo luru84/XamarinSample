8cb6138fc88ac4fee4f5fcca95020828a8dadead
Tatsuya Hashimura <luru84@gmail.com>
2020-08-16 13:22:43 +0900

    [add]refs iPhoneApp追加

---
+++ KitchenTimeriPhone/KitchenTimeriPhone/MyViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace KitchenTimeriPhone
+{
+    public partial class MyViewController : UIViewController
+    {
+        public MyViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
